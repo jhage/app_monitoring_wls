eb3766114085dc7b51eb67e9683a1c90aafb85aa
leandross <lss.aspira@gmail.com>
2018-01-10 23:57:36 -0200

    commit initial

---
+++ src/main/java/br/com/monitoring/wls/StartCompleteWebLogicMonitoring.java
@@ -0,0 +1,11 @@
+package br.com.monitoring.wls;
+
+public class StartCompleteWebLogicMonitoring {
+
+	
+	public	static	void main (String... a ) throws Exception {
+		//CompleteWebLogicMonitoring.main("otp1wl01.internal.timbrasil.com.br", "7007", "capacity", "timbrasil01", "." ,"0", "0", "0", "THREAD_DUMP");
+		CompleteWebLogicMonitoring.main("otp1wl01.internal.timbrasil.com.br", "7007", "capacity", "timbrasil01", "." ,"0", "0", "0", "THREAD_POOL");		
+		
+	}
+}
